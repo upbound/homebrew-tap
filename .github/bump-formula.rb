edf6c3663a3a3eee52f41c4774ece753a4ff7470
Aditya Sharma <git@adi.run>
2022-08-26 15:31:09 -0700

    Use GITHUB_WORKSPACE for repo root
    Signed-off-by: Aditya Sharma <git@adi.run>

--- .github/bump-formula.rb
+++ .github/bump-formula.rb
@@ -5,16 +5,18 @@ ARCHS = %w[darwin_amd64 darwin_arm64 linux_amd64 linux_arm linux_arm64].freeze
 
 OLD_VERSION = ARGV[0]
 NEW_VERSION = ARGV[1]
+REPO_ROOT = ENV['GITHUB_WORKSPACE']
 
-Dir.entries("#{File.dirname(__FILE__)}/Formula/").each do |file|
+Dir.entries("#{REPO_ROOT}/Formula/").each do |file|
   next unless file.end_with?('.rb')
 
   puts "Processing #{file}"
 
   refs = {}
-  formula = file.chomp('.rb')
 
   ARCHS.each do |arch|
+    formula = file.chomp('.rb')
+
     url = "https://cli.upbound.io/stable/#{OLD_VERSION}/bundle/#{formula}/#{arch}.tar.gz"
     old_sha256 = `curl -sSL #{url} | sha256sum | cut -d' ' -f1`.strip
 
@@ -27,13 +29,13 @@ Dir.entries("#{File.dirname(__FILE__)}/Formula/").each do |file|
     }
   end
 
-  text = File.read("#{File.dirname(__FILE__)}/Formula/" + file)
+  text = File.read("#{REPO_ROOT}/Formula/" + file)
   text.gsub!(OLD_VERSION, NEW_VERSION)
   ARCHS.each do |arch|
     text.gsub!(refs[arch][:old_sha256], refs[arch][:new_sha256])
   end
 
-  File.open("#{File.dirname(__FILE__)}/Formula/" + file, 'w') do |f|
+  File.open("#{REPO_ROOT}/Formula/" + file, 'w') do |f|
     f.write(text)
   end
 end
