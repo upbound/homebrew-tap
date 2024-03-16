f2eac41ca724bd0fbc950490e9ee9b58ef5706f1
sttts <sttts@users.noreply.github.com>
2024-03-16 19:43:26 +0000

    Bump forumla versions from `v0.25.0` to `v0.26.0`

--- Formula/up.rb
+++ Formula/up.rb
@@ -17,28 +17,28 @@
 class Up < Formula
   desc 'The official Upbound CLI'
   homepage 'https://upbound.io'
-  version 'v0.25.0'
+  version 'v0.26.0'
   license 'Upbound Software License'
 
   if OS.mac? && Hardware::CPU.intel?
-    url 'https://cli.upbound.io/stable/v0.25.0/bundle/up/darwin_amd64.tar.gz'
-    sha256 '203da59cacc7ba91fa469816bca8d618d0e36270179bd4fd49f16a6be81fec79'
+    url 'https://cli.upbound.io/stable/v0.26.0/bundle/up/darwin_amd64.tar.gz'
+    sha256 '1a33651f3cdd6c743054bc528cf6c78a538f0bdb9a01d3a96587d1d9880c5032'
   end
   if OS.mac? && Hardware::CPU.arm?
-    url 'https://cli.upbound.io/stable/v0.25.0/bundle/up/darwin_arm64.tar.gz'
-    sha256 'b3662f48c786540ccea5ed5467caf4e9d6b9eeb7f0e80dc77970fabd7eb449cd'
+    url 'https://cli.upbound.io/stable/v0.26.0/bundle/up/darwin_arm64.tar.gz'
+    sha256 '1a33651f3cdd6c743054bc528cf6c78a538f0bdb9a01d3a96587d1d9880c5032'
   end
   if OS.linux? && Hardware::CPU.intel?
-    url 'https://cli.upbound.io/stable/v0.25.0/bundle/up/linux_amd64.tar.gz'
-    sha256 'fa87ab91f7a5086df49711e262cc42806692cb353bffbc17635559658b636914'
+    url 'https://cli.upbound.io/stable/v0.26.0/bundle/up/linux_amd64.tar.gz'
+    sha256 '1a33651f3cdd6c743054bc528cf6c78a538f0bdb9a01d3a96587d1d9880c5032'
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url 'https://cli.upbound.io/stable/v0.25.0/bundle/up/linux_arm.tar.gz'
-    sha256 '8f4a4cdff8f91e5092bb74ac206adfc45d4c6903665fe8a8e8148dea151626a4'
+    url 'https://cli.upbound.io/stable/v0.26.0/bundle/up/linux_arm.tar.gz'
+    sha256 '1a33651f3cdd6c743054bc528cf6c78a538f0bdb9a01d3a96587d1d9880c5032'
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url 'https://cli.upbound.io/stable/v0.25.0/bundle/up/linux_arm64.tar.gz'
-    sha256 'df769782f9d1cbce308955e65657498f5870637acf39cf42b1452e9df47845b0'
+    url 'https://cli.upbound.io/stable/v0.26.0/bundle/up/linux_arm64.tar.gz'
+    sha256 '1a33651f3cdd6c743054bc528cf6c78a538f0bdb9a01d3a96587d1d9880c5032'
   end
 
   def install
