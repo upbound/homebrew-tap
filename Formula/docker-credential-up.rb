56587f933e9d586f80f149d2229dcd50247ab69c
Jason Tang <jason@upbound.io>
2024-03-19 09:21:32 -0400

    Revert "Bump forumla versions from `v0.25.0` to `v0.26.0`"
    This reverts commit f2eac41ca724bd0fbc950490e9ee9b58ef5706f1.

--- Formula/docker-credential-up.rb
+++ Formula/docker-credential-up.rb
@@ -17,28 +17,28 @@
 class DockerCredentialUp < Formula
   desc 'Upbound Docker credential helper'
   homepage 'https://upbound.io'
-  version 'v0.26.0'
+  version 'v0.25.0'
   license 'Upbound Software License'
 
   if OS.mac? && Hardware::CPU.intel?
-    url 'https://cli.upbound.io/stable/v0.26.0/bundle/docker-credential-up/darwin_amd64.tar.gz'
-    sha256 '1a33651f3cdd6c743054bc528cf6c78a538f0bdb9a01d3a96587d1d9880c5032'
+    url 'https://cli.upbound.io/stable/v0.25.0/bundle/docker-credential-up/darwin_amd64.tar.gz'
+    sha256 '105e2032ee19c114adb2dffc1913d6c343350afba7032fda10bfc46e756a9b3e'
   end
   if OS.mac? && Hardware::CPU.arm?
-    url 'https://cli.upbound.io/stable/v0.26.0/bundle/docker-credential-up/darwin_arm64.tar.gz'
-    sha256 '1a33651f3cdd6c743054bc528cf6c78a538f0bdb9a01d3a96587d1d9880c5032'
+    url 'https://cli.upbound.io/stable/v0.25.0/bundle/docker-credential-up/darwin_arm64.tar.gz'
+    sha256 '07b3c82a19c19e533af6f28512dcc3ed606e50192b5f85934aa95d4288a37b0a'
   end
   if OS.linux? && Hardware::CPU.intel?
-    url 'https://cli.upbound.io/stable/v0.26.0/bundle/docker-credential-up/linux_amd64.tar.gz'
-    sha256 '1a33651f3cdd6c743054bc528cf6c78a538f0bdb9a01d3a96587d1d9880c5032'
+    url 'https://cli.upbound.io/stable/v0.25.0/bundle/docker-credential-up/linux_amd64.tar.gz'
+    sha256 'c677dfe367c342a0a75dcf8b1deeda72df4793d71dd854010b031a890fc8091f'
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url 'https://cli.upbound.io/stable/v0.26.0/bundle/docker-credential-up/linux_arm.tar.gz'
-    sha256 '1a33651f3cdd6c743054bc528cf6c78a538f0bdb9a01d3a96587d1d9880c5032'
+    url 'https://cli.upbound.io/stable/v0.25.0/bundle/docker-credential-up/linux_arm.tar.gz'
+    sha256 '458bd1b616a42f5d9a28b88df7ec46de321b651a6c99371dd804f56f2c62ab2c'
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url 'https://cli.upbound.io/stable/v0.26.0/bundle/docker-credential-up/linux_arm64.tar.gz'
-    sha256 '1a33651f3cdd6c743054bc528cf6c78a538f0bdb9a01d3a96587d1d9880c5032'
+    url 'https://cli.upbound.io/stable/v0.25.0/bundle/docker-credential-up/linux_arm64.tar.gz'
+    sha256 'c90ec7e5103615361aeca0e570438af5cfdfd30e81df809e428f24ba02423be6'
   end
 
   def install
